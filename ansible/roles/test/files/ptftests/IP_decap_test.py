50a97a6300ea4212b0ea491a7cef6cfe536890ad
vincent201881 <ximeng@celestica.com>
2019-09-12 07:14:31 +0000

    update to the master(...)

--- ansible/roles/test/files/ptftests/IP_decap_test.py
+++ ansible/roles/test/files/ptftests/IP_decap_test.py
@@ -101,6 +101,8 @@ class DecapPacketTest(BaseTest):
             self.src_ports = [0,  1,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 36, 37, 38, 39, 40, 41, 42, 48, 52, 53, 54, 55, 56, 57, 58]
         if self.test_params['testbed_type'] == 't0-116':
             self.src_ports = range(0, 24) + range(32, 120)
+        if self.test_params['testbed_type'] == 't0-52':
+            self.src_ports = range(0, 52)
         if self.test_params['testbed_type'] == 't0-slx':
             self.src_ports = range(1, 25) + range(27, 31)
         if self.test_params['testbed_type'] == 't0-e1031':
